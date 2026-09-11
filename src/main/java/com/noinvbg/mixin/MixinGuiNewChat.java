f6ffb007432a2a05f8d0ff4b634644cd72195a6c
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 15:27:02 +0200

    Update MixinGuiNewChat.java

--- src/main/java/com/noinvbg/mixin/MixinGuiNewChat.java
+++ src/main/java/com/noinvbg/mixin/MixinGuiNewChat.java
@@ -12,12 +12,13 @@ public abstract class MixinGuiNewChat {
         method = "func_146230_a",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/GuiNewChat;drawRect(IIIII)V"
+            target = "Lnet/minecraft/client/gui/GuiNewChat;func_73733_a(IIIII)V"
         )
     )
     private void onDrawChatBackground(int startX, int startY, int endX, int endY, int color) {
         if (!Main.noChatBackground) {
-            net.minecraft.client.gui.GuiNewChat.drawRect(startX, startY, endX, endY, color);
+            // Zavolá pôvodnú metódu drawRect cez jej SRG ekvivalent func_73733_a
+            ((net.minecraft.client.gui.GuiNewChat) (Object) this).func_73733_a(startX, startY, endX, endY, color);
         }
     }
 }
