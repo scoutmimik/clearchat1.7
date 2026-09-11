2ff99b2f657e00de86ee5138439d65e5a66a1e54
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 11:30:55 +0200

    Update and rename NoBackgroundHandler.java to Main.java

--- src/main/resources/noinvbg/Main.java
+++ src/main/resources/noinvbg/Main.java
@@ -2,13 +2,13 @@ package com.noinvbg;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 
@@ -18,31 +18,21 @@ public class Main {
     public static final String NAME = "NoInvBG";
     public static final String VERSION = "1.0";
 
-    // Prepínač pre pozadie inventára (true = bez tmavého pozadia)
     public static boolean noInvBackground = true;
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        // Registrácia eventu pre skrývanie pozadia inventára
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @Mod.EventHandler
-    public void serverStarting(FMLServerStartingEvent event) {
-        // Registrácia príkazov
-        event.registerServerCommand(new CommandToggleNoInvBG());
+        ClientCommandHandler.instance.registerCommand(new CommandToggleNoInvBG());
     }
 
     @SubscribeEvent
     public void onBackgroundDraw(GuiScreenEvent.BackgroundDrawnEvent.Pre event) {
-        // Ak je funkcia zapnutá a hráčka/hráč otvoril GuiContainer (inventár, truhla, crafting...)
         if (noInvBackground && event.gui instanceof GuiContainer) {
-            // Zrušíme vykreslenie tmavého pozadia
             event.setCanceled(true);
         }
     }
 
-    // Príkaz na prepínanie funkcie v hre (/noinvbg)
     public static class CommandToggleNoInvBG extends CommandBase {
         @Override
         public String getCommandName() {
@@ -56,14 +46,14 @@ public class Main {
 
         @Override
         public int getRequiredPermissionLevel() {
-            return 0; // Príkaz môže použiť každý hráč bez OP práv
+            return 0;
         }
 
         @Override
         public void processCommand(ICommandSender sender, String[] args) {
             noInvBackground = !noInvBackground;
             String status = noInvBackground ? EnumChatFormatting.GREEN + "ZAPNUTÉ" : EnumChatFormatting.RED + "VYPNUTÉ";
-            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GRAY + "[Nametag] " + EnumChatFormatting.WHITE + "No Inventory Background je teraz " + status));
+            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GRAY + "[NoInvBG] " + EnumChatFormatting.WHITE + "Pozadie je teraz " + status));
         }
     }
 }
