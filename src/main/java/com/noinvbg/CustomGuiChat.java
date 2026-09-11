0199bb9deb2421ba1e4df39e0648fd8aec7ce1dc
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 16:31:55 +0200

    Update CustomGuiChat.java

--- src/main/java/com/noinvbg/CustomGuiChat.java
+++ src/main/java/com/noinvbg/CustomGuiChat.java
@@ -10,10 +10,10 @@ public class CustomGuiChat extends GuiNewChat {
     }
 
     @Override
-    public void func_73734_a(int left, int top, int right, int bottom, int color) {
+    public void drawRect(int left, int top, int right, int bottom, int color) {
         if (Main.noChatBackground) {
             return;
         }
-        super.func_73734_a(left, top, right, bottom, color);
+        super.drawRect(left, top, right, bottom, color);
     }
 }
