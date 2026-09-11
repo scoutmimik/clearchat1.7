b22c60a197df9789aa4245093e7725bc5cc6cbde
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 15:57:44 +0200

    Update MixinGuiNewChat.java

--- src/main/java/com/noinvbg/mixin/MixinGuiNewChat.java
+++ src/main/java/com/noinvbg/mixin/MixinGuiNewChat.java
@@ -4,20 +4,23 @@ import com.noinvbg.Main;
 import net.minecraft.client.gui.GuiNewChat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiNewChat.class)
 public class MixinGuiNewChat {
 
-    @ModifyArg(
+    @Inject(
         method = "func_146230_a",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/client/gui/Gui;func_73734_a(IIIII)V"
         ),
-        index = 4
+        cancellable = true
     )
-    private int modifyChatBackgroundColor(int color) {
-        return Main.noChatBackground ? 0 : color;
+    private void cancelChatBackground(int updateCounter, CallbackInfo ci) {
+        if (Main.noChatBackground) {
+            ci.cancel();
+        }
     }
 }
