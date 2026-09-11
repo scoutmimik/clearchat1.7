dc33aa66062a7a6302ff023e50095fd743a58f72
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 15:36:07 +0200

    Update MixinGuiNewChat.java

--- src/main/java/com/noinvbg/mixin/MixinGuiNewChat.java
+++ src/main/java/com/noinvbg/mixin/MixinGuiNewChat.java
@@ -17,7 +17,7 @@ public abstract class MixinGuiNewChat {
     )
     private void onDrawChatBackground(int startX, int startY, int endX, int endY, int color) {
         if (!Main.noChatBackground) {
-            net.minecraft.client.gui.Gui.func_73733_a(startX, startY, endX, endY, color);
+            net.minecraft.client.gui.Gui.drawRect(startX, startY, endX, endY, color);
         }
     }
 }
