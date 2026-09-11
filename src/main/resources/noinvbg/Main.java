76616cb9c531d04cb238ad9451314c9831cf7c31
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 12:54:24 +0200

    Update Main.java

--- src/main/resources/noinvbg/Main.java
+++ src/main/resources/noinvbg/Main.java
@@ -1,60 +1,19 @@
-package com.noinvbg;
+package com.noinvbg.mixin;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import net.minecraft.command.CommandBase;
-import net.minecraft.command.ICommandSender;
+import com.noinvbg.Main;
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraftforge.client.ClientCommandHandler;
-import net.minecraftforge.client.event.GuiScreenEvent;
-import net.minecraftforge.common.MinecraftForge;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mod(
-   modid = "noinvbg",
-   version = "1.0",
-   acceptedMinecraftVersions = "[1.7.10]"
-)
-public class Main {
+@Mixin(GuiContainer.class)
+public class MixinGuiContainer {
 
-    public static boolean noInvBackground = true;
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-        MinecraftForge.EVENT_BUS.register(this);
-        ClientCommandHandler.instance.registerCommand(new CommandToggleNoInvBG());
-    }
-
-    @SubscribeEvent
-    public void onBackgroundDraw(GuiScreenEvent.BackgroundDrawnEvent.Pre event) {
-        if (noInvBackground && event != null && event.gui instanceof GuiContainer) {
-            event.setCanceled(true);
-        }
-    }
-
-    public static class CommandToggleNoInvBG extends CommandBase {
-        @Override
-        public String getCommandName() {
-            return "noinvbg";
-        }
-
-        @Override
-        public String getCommandUsage(ICommandSender sender) {
-            return "/noinvbg";
-        }
-
-        @Override
-        public int getRequiredPermissionLevel() {
-            return 0;
-        }
-
-        @Override
-        public void processCommand(ICommandSender sender, String[] args) {
-            noInvBackground = !noInvBackground;
-            String status = noInvBackground ? EnumChatFormatting.GREEN + "ZAPNUTÉ" : EnumChatFormatting.RED + "VYPNUTÉ";
-            sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GRAY + "[NoInvBG] " + EnumChatFormatting.WHITE + "Pozadie je teraz " + status));
+    @Inject(method = "drawDefaultBackground", at = @At("HEAD"), cancellable = true)
+    private void onDrawDefaultBackground(CallbackInfo ci) {
+        if (Main.noInvBackground) {
+            ci.cancel();
         }
     }
 }
