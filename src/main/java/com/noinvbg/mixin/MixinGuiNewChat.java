f9d7b4d71c15834ecd23531cfea3dc042d9e19b6
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 15:42:13 +0200

    Update MixinGuiNewChat.java

--- src/main/java/com/noinvbg/mixin/MixinGuiNewChat.java
+++ src/main/java/com/noinvbg/mixin/MixinGuiNewChat.java
@@ -1,23 +1,23 @@
 package com.noinvbg.mixin;
 
 import com.noinvbg.Main;
+import net.minecraft.client.gui.GuiNewChat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(net.minecraft.client.gui.GuiNewChat.class)
+@Mixin(GuiNewChat.class)
 public abstract class MixinGuiNewChat {
 
-    @Redirect(
+    @ModifyArg(
         method = "func_146230_a",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/Gui;func_73733_a(IIIII)V"
-        )
+            target = "Lnet/minecraft/client/gui/Gui;drawRect(IIIII)V"
+        ),
+        index = 4
     )
-    private void onDrawChatBackground(int startX, int startY, int endX, int endY, int color) {
-        if (!Main.noChatBackground) {
-            net.minecraft.client.gui.Gui.drawRect(startX, startY, endX, endY, color);
-        }
+    private int modifyChatBackgroundColor(int color) {
+        return Main.noChatBackground ? 0 : color;
     }
 }
