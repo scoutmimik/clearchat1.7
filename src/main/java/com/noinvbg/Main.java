78d26111560d790202f901bdbd8ee4977e84ef62
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 16:09:17 +0200

    Update Main.java

--- src/main/java/com/noinvbg/Main.java
+++ src/main/java/com/noinvbg/Main.java
@@ -2,15 +2,12 @@ package com.noinvbg;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.ClientCommandHandler;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
-import net.minecraftforge.common.MinecraftForge;
 
 @Mod(
    modid = "noinvbg",
@@ -26,16 +23,6 @@ public class Main {
     public void init(FMLInitializationEvent event) {
         ClientCommandHandler.instance.registerCommand(new CommandToggleNoInvBG());
         ClientCommandHandler.instance.registerCommand(new CommandToggleNoChatBG());
-        
-        // Registrácia eventu pre chat
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @SubscribeEvent
-    public void onRenderChat(RenderGameOverlayEvent.Pre event) {
-        if (event.type == RenderGameOverlayEvent.ElementType.CHAT && noChatBackground) {
-            // Bezpečné zrušenie vykresľovania chatu bez priameho Mixinu na GuiNewChat
-        }
     }
 
     public static class CommandToggleNoInvBG extends CommandBase {
@@ -81,6 +68,10 @@ public class Main {
         @Override
         public void processCommand(ICommandSender sender, String[] args) {
             noChatBackground = !noChatBackground;
+            
+            // Priama úprava priehľadnosti chatu v nastaveniach Minecraftu
+            Minecraft.getMinecraft().gameSettings.chatOpacity = noChatBackground ? 0.0F : 1.0F;
+
             String status = noChatBackground ? EnumChatFormatting.GREEN + "ON" : EnumChatFormatting.RED + "OFF";
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GRAY + "[NoChatBG] Chat Background: " + status));
         }
