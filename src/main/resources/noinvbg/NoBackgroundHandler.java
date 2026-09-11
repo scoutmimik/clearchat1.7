1188bd0376e086464d602d0ad37573150f04c943
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 11:23:43 +0200

    Update NoBackgroundHandler.java

--- src/main/resources/noinvbg/NoBackgroundHandler.java
+++ src/main/resources/noinvbg/NoBackgroundHandler.java
@@ -14,8 +14,8 @@ import net.minecraftforge.common.MinecraftForge;
 
 @Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION)
 public class Main {
-    public static final String MODID = "nametag";
-    public static final String NAME = "Nametag Mod";
+    public static final String MODID = "noinvbg";
+    public static final String NAME = "NoInvBG";
     public static final String VERSION = "1.0";
 
     // Prepínač pre pozadie inventára (true = bez tmavého pozadia)
