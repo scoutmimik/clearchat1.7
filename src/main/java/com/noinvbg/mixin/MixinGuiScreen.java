7fcc62661c3233ca923b4fdb5c50d6da32c33f95
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 13:57:33 +0200

    Update and rename MixinGuiContainer.java to MixinGuiScreen.java

--- src/main/java/com/noinvbg/mixin/MixinGuiScreen.java
+++ src/main/java/com/noinvbg/mixin/MixinGuiScreen.java
@@ -10,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GuiScreen.class)
 public abstract class MixinGuiScreen {
 
-    @Inject(method = "drawWorldBackground", at = @At("HEAD"), cancellable = true)
-    public void onDrawWorldBackground(int tint, CallbackInfo ci) {
+    @Inject(method = "drawBackground", at = @At("HEAD"), cancellable = true)
+    public void onDrawBackground(int tint, CallbackInfo ci) {
         if (Main.noInvBackground) {
             ci.cancel();
         }
