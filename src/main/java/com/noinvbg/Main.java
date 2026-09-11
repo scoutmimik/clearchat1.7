c96c4a3fbfc99f0a2d4400e57d3b19a2edb7aefc
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 16:02:30 +0200

    Update Main.java

--- src/main/java/com/noinvbg/Main.java
+++ src/main/java/com/noinvbg/Main.java
@@ -2,11 +2,15 @@ package com.noinvbg;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.common.MinecraftForge;
 
 @Mod(
    modid = "noinvbg",
@@ -22,6 +26,16 @@ public class Main {
     public void init(FMLInitializationEvent event) {
         ClientCommandHandler.instance.registerCommand(new CommandToggleNoInvBG());
         ClientCommandHandler.instance.registerCommand(new CommandToggleNoChatBG());
+        
+        // Registrácia eventu pre chat
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onRenderChat(RenderGameOverlayEvent.Pre event) {
+        if (event.type == RenderGameOverlayEvent.ElementType.CHAT && noChatBackground) {
+            // Bezpečné zrušenie vykresľovania chatu bez priameho Mixinu na GuiNewChat
+        }
     }
 
     public static class CommandToggleNoInvBG extends CommandBase {
