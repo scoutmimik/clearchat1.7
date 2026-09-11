890d31610eef6dd38693257137995dc1fac22c1b
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 13:48:45 +0200

    Update MixinGuiContainer.java

--- src/main/java/com/noinvbg/mixin/MixinGuiContainer.java
+++ src/main/java/com/noinvbg/mixin/MixinGuiContainer.java
@@ -1,7 +1,6 @@
 package com.noinvbg.mixin;
 
 import com.noinvbg.Main;
-import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,18 +8,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiContainer.class)
-public abstract class MixinGuiContainer extends GuiScreen {
+public abstract class MixinGuiContainer {
 
-    @Inject(method = "drawScreen", at = @At("HEAD"))
-    public void onDrawScreenHead(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
-        // Tu môžeme zachytiť začiatok
-    }
-
-    // Cielime priamo na metódu drawDefaultBackground, ktorú GuiContainer volá vo svojom drawScreen
-    @Inject(method = "drawDefaultBackground", at = @At("HEAD"), cancellable = true)
-    public void onDrawDefaultBackground(CallbackInfo ci) {
+    @Inject(method = "drawWorldBackground", at = @At("HEAD"), cancellable = true)
+    public.void onDrawWorldBackground(int tint, CallbackInfo ci) {
         if (Main.noInvBackground) {
-            ci.cancel(); // Úplne zruší vykreslenie tmavej/rozmazanej tmavej vrstvy pozadia inventára
+            ci.cancel();
         }
     }
 }
