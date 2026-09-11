cb19a2cd61c68afd831f555e1539ca28821d648e
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 13:55:45 +0200

    Update MixinGuiContainer.java

--- src/main/java/com/noinvbg/mixin/MixinGuiContainer.java
+++ src/main/java/com/noinvbg/mixin/MixinGuiContainer.java
@@ -1,14 +1,14 @@
 package com.noinvbg.mixin;
 
 import com.noinvbg.Main;
-import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.gui.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GuiContainer.class)
-public abstract class MixinGuiContainer {
+@Mixin(GuiScreen.class)
+public abstract class MixinGuiScreen {
 
     @Inject(method = "drawWorldBackground", at = @At("HEAD"), cancellable = true)
     public void onDrawWorldBackground(int tint, CallbackInfo ci) {
