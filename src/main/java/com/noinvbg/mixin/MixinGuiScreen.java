a346eb7e5f9dde594df7f7fef5ef4a4dd87a2e15
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 14:40:57 +0200

    Update MixinGuiScreen.java

--- src/main/java/com/noinvbg/mixin/MixinGuiScreen.java
+++ src/main/java/com/noinvbg/mixin/MixinGuiScreen.java
@@ -1,3 +1,13 @@
+package com.noinvbg.mixin;
+
+import com.noinvbg.Main;
+import net.minecraft.client.gui.GuiScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
 @Mixin(GuiScreen.class)
 public abstract class MixinGuiScreen {
 
