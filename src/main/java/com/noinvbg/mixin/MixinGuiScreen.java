e5f4ff1d5916126745ca4641e56181ba7707795e
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-11 14:43:55 +0200

    Update MixinGuiScreen.java

--- src/main/java/com/noinvbg/mixin/MixinGuiScreen.java
+++ src/main/java/com/noinvbg/mixin/MixinGuiScreen.java
@@ -17,19 +17,17 @@ public abstract class MixinGuiScreen {
             GuiScreen screen = (GuiScreen) (Object) this;
             String className = screen.getClass().getName();
 
-            // Zoznam obrazoviek, ktoré si ponechajú pôvodné pozadie
             boolean isOptionScreen = screen instanceof net.minecraft.client.gui.GuiOptions ||
                                      screen instanceof net.minecraft.client.gui.GuiVideoSettings ||
                                      screen instanceof net.minecraft.client.gui.GuiScreenOptionsSounds ||
-                                     screen instanceof net.minecraft.client.gui.GuiChatSettings || // Pridané chat settings
                                      screen instanceof cpw.mods.fml.client.GuiModList ||
+                                     className.contains("GuiChatSettings") ||
                                      className.contains("GuiDetailSettings") ||
                                      className.contains("GuiQualitySettings") ||
                                      className.contains("GuiPerformanceSettings") ||
                                      className.contains("GuiAnimationSettings") ||
                                      className.contains("GuiOtherSettings");
 
-            // Ak to nie je žiadna z vybratých obrazoviek, pozadie sa zruší.
             if (!isOptionScreen) {
                 ci.cancel();
             }
